c71322a8fef577837d3fec6f69bcd9cf661764a2
Elliot80132654 <elliot.ngwk@gmail.com>
2025-02-02 21:01:13 +0800

    db passoword thing?

--- user-service/database/db.go
+++ user-service/database/db.go
@@ -11,36 +11,7 @@ var DB *sql.DB
 
 func InitDB() {
 	var err error
-<<<<<<< HEAD
-
-	// Read MySQL credentials from environment variables, fallback to default values
-	mysqlUser := os.Getenv("MYSQL_USER")
-	if mysqlUser == "" {
-		mysqlUser = "root"
-	}
-
-	mysqlPassword := os.Getenv("MYSQL_PASS")
-	if mysqlPassword == "" {
-		mysqlPassword = "password" // Default password for others
-	}
-
-	mysqlHost := os.Getenv("MYSQL_HOST")
-	if mysqlHost == "" {
-		mysqlHost = "127.0.0.1"
-	}
-
-	mysqlDatabase := os.Getenv("MYSQL_DBNAME")
-	if mysqlDatabase == "" {
-		mysqlDatabase = "elderly"
-	}
-
-	// Construct DSN (Database Source Name)
-	dsn := mysqlUser + ":" + mysqlPassword + "@tcp(" + mysqlHost + ":3306)/" + mysqlDatabase
-
-	DB, err = sql.Open("mysql", dsn)
-=======
 	DB, err = sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/elderly?parseTime=true&loc=Local")
->>>>>>> a892430054abc094c0711c32ca3f644029111df5
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
